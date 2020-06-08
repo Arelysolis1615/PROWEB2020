1b5b900c1932c5a5f73935b0b3d3dda79843cab5
Arely <Arely_solis1615@outlook.com>
2020-06-08 18:10:30 -0500

    nuevo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -46,7 +46,7 @@ const Footer =(props) =>{
                     <Box p={4} align="justify"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</Box>
                 </Grid>
                 <Grid item xs={12} sm={12} lg={3}>
-                    <Box p={4}> SIGUENOS EN: </Box>
+                    <Box p={4}> SIGUENOS EN : </Box>
                 </Grid>
                 <FaceIcon/>
             </Grid>
